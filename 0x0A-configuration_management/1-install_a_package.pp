528bddeceeedfd572c9a35bdd69d983d6dfb010c
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-06-28 20:10:41 +0000

    install flask using pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,6 @@
-#!/usr/bin/puppet
 # install flask from pip3
-package {'flask':
-
-  ensure   => '2.1.0',
-  provider => 'pip3'
+exec {'install-flask':
+  command => 'pip3 install flask==2.1.0',
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
 

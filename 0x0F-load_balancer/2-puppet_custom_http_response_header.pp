76d0a8e591c62228d64e28d23aeba5dde25118f3
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-07-08 12:41:24 +0100

    configure http response using puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,8 @@
+# http header custom response
+exec { 'install':
+  command => 'apt-get -y update,
+  apt-get -y nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  sudo service nginx restart',
+  provider => shell,
+}

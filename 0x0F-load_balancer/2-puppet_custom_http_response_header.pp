06ba59ae3caec9c3da410a7f4de205a4080d2d06
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-07-10 00:14:53 +0100

    automate header response using puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,7 +1,7 @@
 # http header custom response
-exec { 'install':
-  command  => 'apt-get -y update,
-  apt-get -y nginx;
+exec { 'command':
+  command  => 'sudo apt-get -y update,
+  sudo apt-get -y nginx;
   sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
   sudo service nginx restart',
   provider => shell,

fce9683ebb7fd534c33000db7a59837b050e14e6
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-07-29 10:29:21 +0100

    random RESTAPI

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,7 +15,7 @@ base_url = "https://jsonplaceholder.typicode.com/users/"
 if __name__ == "__main__":
     user_id = sys.argv[1]
     user = requests.get(base_url + user_id).json()
-    todo = requests.get(base_url + user_id + "/todos", headers=headers).json()
+    todo = requests.get(base_url + user_id + "/todos/", headers=headers).json()
     done = [task for task in todo if task.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(done), len(todo)))

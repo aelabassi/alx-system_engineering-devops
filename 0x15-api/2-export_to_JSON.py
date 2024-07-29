5715aa383472fcf143de233ad59230c88350487c
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-07-29 10:39:55 +0100

    export the result into a json file

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Send request ton RESTAPI
+retrieve employees and tasks"""
+import requests
+import sys
+import json
+
+headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "User-Agent": "Holberton School"
+}
+
+base_url = "https://jsonplaceholder.typicode.com/users/"
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    user = requests.get(base_url + user_id).json()
+    todo = requests.get(base_url + user_id + "/todos/", headers=headers).json()
+    done = [task for task in todo if task.get("completed") is True]
+    with open(user_id + ".json", "w") as f:
+        json.dump({user_id: [{
+            "task": task.get("title"),
+            "completed": task.get("completed"),
+            "username": user.get("username")
+        } for task in todo]}, f)

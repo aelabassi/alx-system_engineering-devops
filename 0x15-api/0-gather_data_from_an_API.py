c19acd618c1b32e89bc6def978850e4429e9464e
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-07-29 10:15:44 +0100

    random RESTAPI

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,12 +4,18 @@ retrieve employees and tasks"""
 import requests
 import sys
 
+headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "User-Agent": "Holberton School"
+}
+
 base_url = "https://jsonplaceholder.typicode.com/users/"
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
     user = requests.get(base_url + user_id).json()
-    todo = requests.get(base_url + user_id + "/todos").json()
+    todo = requests.get(base_url + user_id + "/todos", headers=headers).json()
     done = [task for task in todo if task.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(done), len(todo)))

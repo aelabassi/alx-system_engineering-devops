8a74b5051453b02a9a5775a84b3d9fd5aa2d6eb3
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-08-13 10:07:48 +0100

    number of subs in a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Get the number of subscribers in a subreddit """
+import requests
+import sys
+
+
+def get_number_subscribers(subreddit):
+    """ gets the number of sub scribers in a subreddit """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        'User-Agent': 'Mozilla/5.0'
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    data = response.json()
+    return data['data']['subscribers']

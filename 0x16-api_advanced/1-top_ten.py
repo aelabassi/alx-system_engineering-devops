0039a5efc3c41a2de2f9825c3880c1bd4f75953c
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-08-13 10:47:32 +0100

    ten hot titles in a subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" top 10 hot posts in a subreddit """
+import requests
+
+
+def top_ten(subreddit):
+    """ gets the top 10 hot posts in a subreddit """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {
+        'User-Agent': 'Mozilla/5.0'
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print("None")
+        return
+    data = response.json()
+    posts = data['data']['children']
+    for post in posts:
+        print(post['data']['title'])
